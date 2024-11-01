f7dfa7e85d9c988d43fd125134731da801fda2e5
Gus <gusmaimoun@gmail.com>
2024-11-01 17:59:16 -0400

    version-2

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let [seconds, minutes, hours] = [0, 0, 0];
 let timerRef = document.querySelector('.timerDisplay');
 let int = null;
+let submit = document.getElementById('form-button');
+
+let inputHours = document.getElementById('form-hours');
+let inputMinutes = document.getElementById('form-minutes');
+let inputSeconds = document.getElementById('form-seconds');
 
 document.getElementById('startTimer').addEventListener('click', () => {
   if (int !== null) {
     clearInterval(int);
   }
-  int = setInterval(displayTimer, 10);
+  int = setInterval(displayTimer, 1000);
 });
 
 document.getElementById('pauseTimer').addEventListener('click', () => {
@@ -15,34 +20,104 @@ document.getElementById('pauseTimer').addEventListener('click', () => {
 
 document.getElementById('resetTimer').addEventListener('click', () => {
   clearInterval(int);
-  [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-  timerRef.innerHTML = '00 : 00 : 00 : 000 ';
+  [seconds, minutes, hours] = [0, 0, 0];
+  timerRef.innerHTML = '00 : 00 : 00';
 });
 
 function displayTimer() {
-  milliseconds += 10;
-  if (milliseconds == 1000) {
-    milliseconds = 0;
-    seconds++;
-    if (seconds == 60) {
-      seconds = 0;
-      minutes++;
-      if (minutes == 60) {
-        minutes = 0;
-        hours++;
-      }
+  seconds++;
+  if (seconds == 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes == 60) {
+      minutes = 0;
+      hours++;
     }
   }
 
   let h = hours < 10 ? '0' + hours : hours;
   let m = minutes < 10 ? '0' + minutes : minutes;
   let s = seconds < 10 ? '0' + seconds : seconds;
-  let ms =
-    milliseconds < 10
-      ? '00' + milliseconds
-      : milliseconds < 100
-      ? '0' + milliseconds
-      : milliseconds;
 
-  timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
+  timerRef.innerHTML = ` ${h} : ${m} : ${s}`;
 }
+
+function displayTimerReverse() {
+  if (hours === 0 && minutes === 0 && seconds === 0) {
+    clearInterval(int);
+    alert("Time's up!");
+    return;
+  }
+
+  if (seconds > 0) {
+    seconds--;
+  } else {
+    if (minutes > 0) {
+      seconds = 59;
+      minutes--;
+    } else if (hours > 0) {
+      seconds = 59;
+      minutes = 59;
+      hours--;
+    } else {
+      clearInterval(int);
+      alert("Time's up!");
+      return;
+    }
+  }
+
+  let h = hours < 10 ? '0' + hours : hours;
+  let m = minutes < 10 ? '0' + minutes : minutes;
+  let s = seconds < 10 ? '0' + seconds : seconds;
+
+  timerRef.innerHTML = `${h} : ${m} : ${s}`;
+}
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  hours = +inputHours.value;
+  minutes = +inputMinutes.value;
+  seconds = +inputSeconds.value;
+  if (hours === 0 && minutes === 0 && seconds === 0) {
+    clearInterval(int);
+    [seconds, minutes, hours] = [0, 0, 0];
+    timerRef.innerHTML = '00 : 00 : 00';
+    inputHours.value = '';
+    inputMinutes.value = '';
+    inputSeconds.value = '';
+    return;
+  }
+
+  if (
+    hours < 0 ||
+    hours > 24 ||
+    minutes < 0 ||
+    minutes > 60 ||
+    seconds < 0 ||
+    seconds > 60
+  ) {
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    inputHours.value = '';
+    inputMinutes.value = '';
+    inputSeconds.value = '';
+    alert(`Dear user,
+
+      Please insert values in the following range:
+
+      Hours between 0 and 24
+      Minutes between 0 and 60
+      Seconds between 0 and 60
+
+      Thanks & enjoy ❤️`);
+    return;
+  }
+  if (int !== null) {
+    clearInterval(int);
+  }
+  int = setInterval(displayTimerReverse, 1000);
+  inputHours.value = '';
+  inputMinutes.value = '';
+  inputSeconds.value = '';
+});
